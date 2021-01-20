5a2d878b2f9c9def9b6629bfbf985e6381a6a811
Danylo Savchak <daniel.savchak@gmail.com>
2021-01-20 10:49:45 +0200

    create RouteController with all need methods in dao & service

--- tradoxCode/src/main/java/com/nc/tradox/api/RouteController.java
+++ tradoxCode/src/main/java/com/nc/tradox/api/RouteController.java
@@ -1,9 +1,15 @@
 package com.nc.tradox.api;
 
+import com.nc.tradox.model.InfoData;
 import com.nc.tradox.model.Route;
+import com.nc.tradox.model.User;
 import com.nc.tradox.service.TradoxService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
+import java.util.Set;
 
 @RequestMapping("api/v1/route")
 @RestController
@@ -17,7 +23,27 @@ public class RouteController {
     }
 
     @GetMapping
-    public Route getRoute() {
-        return  tradoxService.getRoute();
+    public Route getRoute(@RequestParam String destinationId, HttpSession session) {
+        if (!(Boolean) session.getAttribute("authorized")) {
+            return null;
+        }
+        Route route = tradoxService.getRoute((String) session.getAttribute("userId"),destinationId);
+        session.setAttribute("currentRoute",route);
+        return route;
     }
+
+    @GetMapping("/save")
+    public Boolean saveRoute(HttpSession session) {
+        Route r  = (Route) session.getAttribute("currentRoute");
+        int userId = (int) session.getAttribute("userId");
+        return tradoxService.saveRoute(r,userId);
+    }
+
+    @PostMapping("/editTransits")
+    public void editTransits(@RequestBody Set<InfoData> transits, HttpSession session) {
+        Integer routeId = ((Route) session.getAttribute("currentRoute")).getElementId();
+        tradoxService.editTransits((int) session.getAttribute("userId"),routeId,transits);
+    }
+
+
 }
--- tradoxCode/src/main/java/com/nc/tradox/service/TradoxService.java
+++ tradoxCode/src/main/java/com/nc/tradox/service/TradoxService.java
@@ -1,28 +1,59 @@
 package com.nc.tradox.service;
 
 import com.nc.tradox.dao.Dao;
-import com.nc.tradox.model.Country;
-import com.nc.tradox.model.Route;
-import com.nc.tradox.model.Status;
-import com.nc.tradox.model.User;
+import com.nc.tradox.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
-@Service //EQUAL TO COMPONENT BUT FOR SPECIFICATE USE THIS
+@Service
 public class TradoxService {
 
     private final Dao dao;
 
-    @Autowired // автоматически устанавливает значение поля
+    @Autowired
     public TradoxService(@Qualifier("oracle") Dao dao) {
         this.dao = dao;
     }
 
-    public Route getRoute() {
-        return dao.getRoute();
+    public Route getRoute(String userId, String destinationId) {
+        return dao.getRoute(userId, destinationId);
+    }
+
+    public Boolean saveRoute(Route route, int userId) {
+        return dao.saveRoute(route,userId);
+    }
+
+    public void editTransits(int userId,Integer route_id, Set<InfoData> transits) {
+        int newOrder = 1;
+        while (transits.iterator().hasNext()) {
+            InfoData infoData = transits.iterator().next();
+            // check if this transit for this route and user already exists with this number
+            ResultSet resultSet  = dao.transitFor(route_id, infoData);
+            int transit_id = -1;
+            int order = -1;
+            try {
+                transit_id = resultSet.getInt("transit_id");
+                order = resultSet.getInt("order");
+            } catch (SQLException e) {
+            }
+            if ((transit_id != -1)) {
+                // if exists but wrong order change the order
+                if (newOrder != order) {
+                    ResultSet resultSet1 = dao.changeTransitOrder(transit_id, newOrder);
+                }
+            } else {
+                // if dont exists create new
+                dao.createNewTransit(newOrder,infoData.getDestinationCountry().getDestinationCountry().getShortName(),route_id);
+            }
+            newOrder++;
+        }
     }
 
     public User auth(String email, String password) {
