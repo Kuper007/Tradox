18b35ea5ea77e4e7bbd01203daa3bab118c0f0fc
Danylo Savchak <daniel.savchak@gmail.com>
2021-01-24 17:00:32 +0200

    add RegistrationScreen but not full

--- tradoxCode/src/main/frontEnd/tradox/src/App.js
+++ tradoxCode/src/main/frontEnd/tradox/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import MainPage from './component/MainPage/MainPage';
 import {Route, BrowserRouter} from 'react-router-dom';
 import WorldMap from "./component/MainPage/WorldMap/WorldMap";
+import Register from './component/RegisterForm/Register.js';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className = 'container-main'>
-        <MainPage/>
+      <div className = '
+      container-main'>
+        {/*<MainPage/>*/}
          {/*<Route path = '' component = {RegisterForm}/> */}
+         <Register/>
       </div>
     </BrowserRouter>
   )
---
+++ tradoxCode/src/main/frontEnd/tradox/src/component/CountrysMap.js
@@ -0,0 +1,14 @@
+let myMap = new Map().set("Andorra","AD").set("United Arab Emirates","AE").set("Afganistan","AF")
+        .set("Antigua and Barbuda","AG").set("Albania","AL").set("Armenia","AM")
+        .set("Angolla","AO").set("Argentina","AR").set("American Samoa", "AS")
+        .set("Austria","AT").set("Australia","AU").set("Azerbaijan","AZ")
+        .set("Bosnia","BA").set("Barbaros","BB").set("Bangladesh","BD")
+        .set("Belgium","BE").set("Burkina Faso","BF").set("Bulgaria","BG")
+        .set("Bahrain","BH").set("Burundi","BI").set("Benin","BJ")
+        .set("Saint Bartelemy","BL").set("Brunei Barussalam","BN").set("Bolivia","BO")
+        .set("Bermuda","BM").set("Bonaire","BQ").set("Brazil","BR")
+        .set("Bahams","BS").set("Bhutan","BT").set("Bouvete Island","BV")
+        .set("Botswana","BW").set("Belarus","BY").set("Belize","BZ")
+        .set("Canada","CA").set("Cocos Islands","CC").set("Democratic Republic of Congo","CD");
+
+export default myMap;
