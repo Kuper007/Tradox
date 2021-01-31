880d44fe7256ce2595138fdb3becd17380e4eaa4
Vadym <fareqvv@gmail.com>
2021-01-31 18:16:46 +0200

    Change search ber color if incorrect country

--- tradoxCode/src/main/frontEnd/tradox/src/component/MainPage/SearchBar/SearchBar.js
+++ tradoxCode/src/main/frontEnd/tradox/src/component/MainPage/SearchBar/SearchBar.js
@@ -1,15 +1,18 @@
-import React from 'react';
 import styles from './SearchBar.module.css'
+import React from 'react'
 
 function SearchBar(props){
 
+    const before = styles.searcher
+    const after  = styles.searcherShake
 
-        return (
-            <div className = {styles.container}>
-                <input type = "text" placeholder="Country Name..." name = 'search' className = {styles.searcher}  onChange = {props.handleCountry} onKeyPress = {props.handleKeyPress}/>
-            </div>
-        );
-    }
+    return (
+        <div className = {styles.container}>
+            {!props.notfound? <input type = "text" placeholder="Country Name..." name = 'search' className = {before}  onChange = {props.handleCountry} onKeyPress = {props.handleKeyPress}/>
+                :<input type = "text" placeholder="Country Name..." name = 'search' className = {after}  onChange = {props.handleCountry} onKeyPress = {props.handleKeyPress}/>}
+        </div>
+    );
+}
 
 
 
