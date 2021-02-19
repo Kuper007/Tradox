22dc02b9261574f0fafaeaaab7a2b1d265d6d786
afanasievbogdan <lee.vong123@gmail.com>
2021-02-19 19:24:15 +0200

    news api and fill db rework

--- tradoxCode/src/test/java/com/nc/tradox/FillDBTest/FillDBTest.java
+++ tradoxCode/src/test/java/com/nc/tradox/FillDBTest/FillDBTest.java
@@ -299,10 +299,10 @@ public class FillDBTest {
                 int count = 0;
                 Statement statement = tradoxDataAccessService.connection.createStatement();
                 for (NewsItem newsItem: newsItemList) {
-                    String haveDocumentQuery = "INSERT INTO NEWS(TEXT, DATE, COUNTRY_ID) " +
-                            "VALUES ('" + newsItem.getText() + "', " +
-                            newsItem.getDate() + ", '" +
-                            newsItem.getCountry() + "')";
+                    String haveDocumentQuery = "INSERT INTO NEWS(TEXT, \"DATE\", COUNTRY_ID) " +
+                            "VALUES ('" + newsItem.getText().replace("'", " ") + "', " +
+                            "TO_DATE(sysdate, 'YYYY-MM-DD HH24:MI:SS')" + ", '" +
+                            newsItem.getCountry().getShortName() + "')";
                     System.out.println(haveDocumentQuery);
                     statement.executeUpdate(haveDocumentQuery);
                     count++;
